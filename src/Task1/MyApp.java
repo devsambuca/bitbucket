14aa3c19e3a21e7ac5031aebbd8a0d073adb8151
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-08-25 19:03:18 +0300

    Adding file Readme for task 1,2

--- src/Task1/MyApp.java
+++ src/Task1/MyApp.java
@@ -7,21 +7,37 @@ public class MyApp {
     public static void main(String[] args) {
         MyCollection mc = new MyCollection();
         MyCollection mc2 = new MyCollection(12);
-        mc2.add(null);
+     //   mc2.add(null);
         mc2.add(1);
+        System.out.println(mc2);
         mc2.add(1);
+        System.out.println(mc2);
         mc2.add(1);
+        System.out.println(mc2);
         mc2.add(1);
+        System.out.println(mc2);
         mc2.add(1);
+        System.out.println(mc2);
         mc2.add(1);
+        System.out.println(mc2);
         mc2.add(1);
+        System.out.println(mc2);
         mc2.add(1);
+        System.out.println(mc2);
+        mc2.remove((Integer) 1);
+        System.out.println(mc2);
         mc2.add(1);
+        System.out.println(mc2);
+        mc2.remove((Integer) 2);
+        System.out.println(mc2);
         mc2.add(1);
-        mc2.add(1);
-        mc2.add(1);
+        System.out.println(mc2);
         System.out.println(mc2.size());
 
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
 
 
         mc.add(12);
@@ -42,7 +58,7 @@ public class MyApp {
         System.out.println(mc);
         mc.add(1);
         System.out.println(mc);
-        System.out.println(mc2.size());
+        System.out.println(mc.size());
 
 
 
@@ -51,6 +67,12 @@ public class MyApp {
         System.out.println("Average " + mc.getAverage());
         System.out.println("MAX " + mc.maxValue());
         System.out.println("MIN " + mc.minValue());
+        System.out.println();
+        System.out.println("value  " + mc2.getIndex(7));
+        System.out.println("index " + mc2.indexOf(-6));
+        System.out.println("Average 2 " + mc2.getAverage());
+        System.out.println("MAX " + mc2.maxValue());
+        System.out.println("MIN " + mc2.minValue());
 
     }
 }
